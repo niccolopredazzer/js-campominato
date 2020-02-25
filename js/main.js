27112470495c21f8fc23b1bef9ea978ece5d7feb
Niccolò Predazzer <niccolo.predazzer@gmail.com>
2020-02-25 17:03:18 +0100

    Manca conteggio utente

---
+++ js/main.js
@@ -0,0 +1,38 @@
+//il computer genera 16 numeri da 1 a 100
+//l'utente inserisce un numero da 1 a 100
+//se il numero è nella lista dei 16 random il gioco termina, altrimenti si chiede all'utente un altro numero
+//la partita termina quando l'utente inserisce uno dei 16 numeri random oppure se li inserisce tutti corretti (tutti tranne quei 16)
+//in seguito stampiamo quanti numeri sono stati inseriti da un utente
+
+var listaNumeriSfortunati = [];               //creo il mio array vuoto che verrà riempito dai 16 numeri maledetti
+
+for (var i = 0; i < 16; i++) {                //questo ciclo mi consente di riempire l'array listaNumeriSfortunati con 16 numeri random da 1 a 100
+    var numeroSfortunato = generaRandomMinMax(1, 100);
+    listaNumeriSfortunati.push(numeroSfortunato);
+}
+console.log(listaNumeriSfortunati);
+
+var numeroInserito = parseInt(prompt('inserisci un numero'));
+
+for (var i = 0; i < 84; i++) {                 //questo ciclo for mi serve per permettere all'utente di inserire il numero massimo di numeri corretti (84)
+    if (listaNumeriSfortunati.includes(numeroInserito)) {
+        console.log('Mi dispiace il gioco è terminato, hai perso');
+    }else {
+        var numeroInserito = parseInt(prompt('inserisci un altro numero'));
+    }
+}
+
+
+
+/*function trovaNumeroArray(numero) {
+    if (array.includes(numero)) {
+        return true;
+    }else {
+        return false;
+    }
+}
+*/
+function generaRandomMinMax(min, max) {
+    var numeroRandom = Math.floor(Math.random() * (max - min + 1) ) + min;
+    return numeroRandom;
+}
