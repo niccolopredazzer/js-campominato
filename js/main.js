33bbcf63f84a8306012d1813de8946d964ccf439
Niccolò Predazzer <niccolo.predazzer@gmail.com>
2020-02-26 17:16:07 +0100

    Inseriti i controlli e la parte bonus

--- js/main.js
+++ js/main.js
@@ -7,10 +7,10 @@
 var listaNumeriSfortunati = [];                //creo il mio array vuoto che verrà riempito dai 16 numeri maledetti
 var dimCampo = sceltaDifficolta();
 var totaleMine = 16;
-var sentinella = false;
+
 
 for (var i = 0; i <= totaleMine; i++) {                //questo ciclo mi consente di riempire l'array listaNumeriSfortunati con 16 numeri random da 1 a 100
-    var numeroSfortunato = generaRandomMinMax(1, 100);
+    var numeroSfortunato = generaRandomMinMax(1, dimCampo);
     if (!listaNumeriSfortunati.includes(numeroSfortunato)) {
         listaNumeriSfortunati.push(numeroSfortunato);
     }
@@ -20,38 +20,45 @@ console.log(listaNumeriSfortunati);
 
 
 var numeriDatiDaUtente = [];                   //questo array vuoto verrà riempito dai numeri inseriti dall'utente e mi serve per capire quanti ne ha inseriti grazie a length
-
-
-while (numeriDatiDaUtente.length <= dimCampo - totaleMine) {
+var boom = false;
+while ((numeriDatiDaUtente.length <= dimCampo - totaleMine) && (boom == false)) {
     var numeroInserito = parseInt(prompt('inserisci un numero tra 1 e ' + dimCampo));
-    if (true) {
-
-    }
-        if (listaNumeriSfortunati.includes(numeroInserito)) {
-            console.log('Mi dispiace il gioco è terminato, hai perso');
-            break;
-        } else if (numeriDatiDaUtente.includes(numeroInserito)) {
-            var numeroInserito = parseInt(prompt('inserisci un numero diverso'));
-        } else if (numeriDatiDaUtente.length == dimCampo - totaleMine) {
-            console.log('hai vinto');
-            break;
-        } else {
-            numeriDatiDaUtente.push(numeroInserito);
-        }
+    if (!isNaN(numeroInserito)) {
+       if (numeroInserito > 0 && numeroInserito <= dimCampo) {
+           if (!numeriDatiDaUtente.includes(numeroInserito)) {
+               if (!listaNumeriSfortunati.includes(numeroInserito)) {
+                   numeriDatiDaUtente.push(numeroInserito);
+                   if (numeriDatiDaUtente.length == dimCampo - totaleMine) {
+                       alert('hai vinto');
+                   }
+               } else {
+                   alert('Mi dispiace il gioco è terminato, hai perso');
+                   boom = true;
+               }
+           } else {
+               alert('inserisci un numero diverso')
+           }
+       } else {
+           alert('inserisci un numero compreso tra 1 e ' + dimCampo);
+       }
+   } else {
+       alert('questo non è un numero, per favore inserisci un numero')
+   }
 }
+
 console.log('Il numero di volte che l\'utente ha inserito un numero giusto è: ' + numeriDatiDaUtente.length);
 
 function sceltaDifficolta() {
     var scelta = parseInt(prompt('Inserisci la difficoltà tra 1, 2 o 3'));
     switch (scelta) {
         case 1:
-            var dimCampo = 100;
+            var dimCampo = 50;
             break;
         case 2:
             var dimCampo = 80;
             break;
         case 3:
-            var dimCampo = 50;
+            var dimCampo = 100;
             break;
         default:
             var dimCampo = 100;
